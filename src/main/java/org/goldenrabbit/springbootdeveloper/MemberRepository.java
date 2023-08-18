1a197a832b4641442ac3afa993dec921cbbb126f
lkdcode <dlrlejr1@naver.com>
2023-08-18 10:51:20 +0900

    [test] chapter4 : Add study

---
+++ src/main/java/org/goldenrabbit/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.goldenrabbit.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
