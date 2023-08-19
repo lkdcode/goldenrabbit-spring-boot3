f5cbedbfe028f7462866392f6c4bedfbce584ddf
lkdcode <dlrlejr1@naver.com>
2023-08-19 14:16:11 +0900

    [feat] user : Add repository

---
+++ src/main/java/org/goldenrabbit/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.goldenrabbit.springbootdeveloper.repository;
+
+import org.goldenrabbit.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
